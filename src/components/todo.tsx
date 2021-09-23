2c55a92240aa9fdada40ef4240c6d9de39dc2863
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 19:37:12 -0400

    Added info and Week 3 TODO

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -4,6 +4,52 @@ function toDo(){
     return(
         <div>
             <h1> TODO </h1>
+
+            <h3>Week #3 TODO</h3>
+            <div>
+                <ul>
+                    <li className="done"> Practice more HTML and read on more advance tags </li>
+                    <li className="done"> Start implementing CSS styles to Portfolio website (Want to check <a href="https://www.youtube.com/watch?v=yfoY53QXEnI" target="_blank" rel="noreferrer">this</a> video out later) </li>
+                    <li className="done"> Read and watch more tutorials on Javascript/Typescript </li>
+                    <li> Read and watch tutorials for React and hooks </li>
+                    <li className="done"> Read and watch tutorials for Boostrap (Maybe watch <a href="https://www.youtube.com/watch?v=k32voqQhODc" target="_blank" rel="noreferrer">this</a> video for flexboxing) </li>
+                    <li> Start flashcard game website with Typescript/CSS/HTML and start section with links for future projects </li>
+                    <li> Implementation of more advance techniques and functionality like Javascript/Typescript</li>
+                    <li> Contemplating the possibility of making a Discord bot on Typescript and create a webpage to add the bot to a private server using APIs</li>
+                    <li> Import this website to a React App</li>
+                    <li> Start exploring the possibility of doing Git Issues and branching</li>
+                    <li> . . .</li>
+                </ul>
+            </div>
+
+            <h3>Week #2 TODO</h3>
+            <div>
+                <ul>
+                    <li className="done"> Practice more HTML and read on more advance tags </li>
+                    <li className="done"> Start implementing CSS styles to Portfolio website (Want to check <a href="https://www.youtube.com/watch?v=yfoY53QXEnI" target="_blank" rel="noreferrer">this</a> video out later) </li>
+                    <li className="done"> Read and watch more tutorials on Javascript/Typescript </li>
+                    <li> Read and watch tutorials for React and hooks </li>
+                    <li className="done"> Read and watch tutorials for Boostrap (Maybe watch <a href="https://www.youtube.com/watch?v=k32voqQhODc" target="_blank" rel="noreferrer">this</a> video for flexboxing) </li>
+                    <li> Start flashcard game website with Typescript/CSS/HTML and start section with links for future projects </li>
+                    <li> Implementation of more advance techniques and functionality like Javascript/Typescript</li>
+                    <li> NEW! Contemplating the possibility of making a Discord bot on Typescript and create a webpage to add the bot to a private server using APIs</li>
+                    <li> NEW! Import this website to a React App</li>
+                    <li> NEW! Start exploring the possibility of doing Git Issues and branching</li>
+                </ul>
+            </div>
+
+            <h3>Week #1 TODO</h3>
+            <div>
+                <ul>
+                    <li> Practice more HTML and read on more advance tags </li>
+                    <li> Start implementing CSS styles to Portfolio website (Want to check <a href="https://www.youtube.com/watch?v=yfoY53QXEnI" target="_blank" rel="noreferrer">this</a> video out later) </li>
+                    <li> Read and watch more tutorials on Javascript/Typescript </li>
+                    <li> Read and watch tutorials for React and hooks </li>
+                    <li> Read and watch tutorials for Boostrap (Maybe watch <a href="https://www.youtube.com/watch?v=k32voqQhODc" target="_blank" rel="noreferrer">this</a> video for flexboxing) </li>
+                    <li> Start flashcard game website with Typescript/CSS/HTML and start section with links for future projects </li>
+                    <li> Implementation of more advance techniques and functionality like Javascript/Typescript</li>
+                </ul>
+            </div>
         </div>
     )
 }
