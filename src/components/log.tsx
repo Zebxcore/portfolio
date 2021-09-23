af6e3990cf827d375563f95641c1b8439cec15f8
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 19:27:13 -0400

    Added and modified Log

--- src/components/log.tsx
+++ src/components/log.tsx
@@ -4,6 +4,73 @@ function log(){
     return(
         <div>
             <h1> Log </h1>
+
+            <div>
+
+                <h3>
+                    09/23/2021
+                </h3>
+                <ul>
+                    <li> Exported website to a React App</li>
+                    <li> Added some info to projects</li>
+                    <li> Created and added repos and React apps for projects</li>
+                </ul>
+
+                <h3>
+                    09/16/2021
+                </h3>
+                <ul>
+                    <li> Expanded TODO and Change Log to their own HTML for better control of length of text</li>
+                    <li> Started React App for webpage</li>
+                </ul>
+                
+                <h3>
+                    09/15/2021
+                </h3>
+                <ul>
+                    <li> Complete overhaul of the website's design</li>
+                </ul>
+        
+                <h3>
+                    09/13/2021
+                </h3>
+                <ul>
+                    <li> Split index.html into more files and changed layout even more with CSS</li>
+                </ul>
+        
+                <h3>
+                    09/12/2021
+                </h3>
+                <ul>
+                    <li> Better formatting style like containers, IDs and classes for different elements</li>
+                </ul>
+        
+                <h3>
+                    09/11/2021
+                </h3>
+                <ul>
+                    <li> Formated text better with more HTML tags</li>
+                    <li> Started adding CSS to some elements on my page like title, images and intro </li>
+                </ul>
+        
+                <h3>
+                    09/09/2021
+                </h3>
+                <ul>   
+                    <li> Added Portfolio, ChangeLog and TODO </li>
+                    <li> Corrections made to webpage intro and index.html </li>
+                    <li> Added info to Milestone Week #1 </li>
+                    <li> Added picture of myself </li>
+                    <li> Gave color to text and background </li>
+                </ul>
+        
+                <h3>
+                    09/03/2021
+                </h3>
+                <ul>
+                    <li> Initial GitHub repository and webpage created </li>
+                </ul>
+            </div>
         </div>
     )
 }
