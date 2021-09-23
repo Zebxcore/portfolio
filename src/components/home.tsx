e1f9c995823c07bcfbe3d2cba3f3d10dabe96093
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 18:26:34 -0400

    Added basic info and image

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import profile from '../assets/profile.png';
 
 function home(){
     return(
         <div>
-            <h1> CISC275 Intro to Software Engineering</h1>
+            <h1> CISC275 - Intro to Software Engineering </h1>
+        
+
+            <div>
+                <div className="row">
+                    <div className="col">
+                        <p> 
+                            This is Sebastian Cortes' portfolio for CISC275 - Intro to Software Engineering <br /><br />
+                            You can find a link to my repository <a href="https://github.com/Zebxcore/Zebxcore.github.oi" target="_blank "> here</a>. 
+                            You can reach me at scortesz@udel.edu, I will be updating this website regularly so expect bugs everywhere :) (or not lol)
+                        </p>
+                    </div>
+                </div>
+                <div className="col">
+                    <div>
+                        <img src={profile} alt="Highschool Graduation, 2017" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
