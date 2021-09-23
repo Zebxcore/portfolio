0c7a15677e0167acfa1af88465b79e4846e02eae
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 19:15:53 -0400

    Split home in two files for better NavBar position

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import aboutMe from './components/about-me';
 import portfolio from './components/portfolio';
 import projects from './components/projects';
 import toDo from './components/todo';
-import home from './components/home';
+import homeTop from './components/home-top';
+import homeBottom from './components/home-bottom';
 import NavBar from './components/navbar';
 import { Route } from 'react-router';
 
@@ -13,9 +14,12 @@ function App() {
   return (
     <div className="App">
 
+      <Route exact path="/home" component={homeTop} />
+      
       <NavBar />
 
-      <Route exact path="/home" component={home} />
+      <Route exact path="/home" component={homeBottom} />
+
       <Route exact path="/about-me" component={aboutMe} /> 
       <Route exact path="/inDepthPortfolio" component={portfolio} /> 
       <Route exact path="/projects" component={projects} /> 
