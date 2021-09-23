841f6492e8a564818b337f83fff07b4d7b77fc83
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 19:30:34 -0400

    Added some info about projects and developmet

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,6 +4,16 @@ function projects(){
     return(
         <div>
             <h1> Projects </h1>
+
+            <p>
+                All my projects for CISC275 and personal projects will be displayed here with their respective links to their repositories as well as their websites.
+            </p>
+
+            <ul>
+                <li> Flashcard Game that will teach Languages while having fun! Link to the website <a href="">here</a> and the repo <a href="https://github.com/Zebxcore/Flashcard-game.github.io"> here</a>. (In development)</li>
+                <li> Discord Bot that will reproduce music by using APIs! Link to the website <a href="">here</a> and the repo <a href="https://github.com/Zebxcore/Saory-the-bot"> here</a>. (In development)</li>
+                <li>Project 3</li>
+            </ul>
         </div>
     )
 }
