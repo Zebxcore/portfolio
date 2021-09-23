04ee333909c9f7bcacfd9d6eabd2d91b51008d4d
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 19:31:51 -0400

    Added basic info about myself

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -4,6 +4,16 @@ function aboutMe(){
     return(
         <div>
             <h1> About Me </h1>
+
+            <p>
+                My name is Sebastian Cortes. I am 24 years old and I am from Colombia. <br />
+                I major in Electrical Engineering but I developed a passion for coding so I decided to take a minor in Computer Science.
+                As of September, 2021, I am a Senior and about to go through a major change in lifestyle after I graduate next Spring.<br />
+                In hopes of findign a job and produce for a couple years, I have been developed my skills in many different areas and skills across different fields 
+                in order to provide my best to my future employers, and employees if I ever decide to open a company myself.<br /><br />
+                <strong>I will update this section regularly just to give an insight of who I am, what my life expectations are, my dreams, wishes and more. :) </strong>
+            </p>
+
         </div>
     )
 }
