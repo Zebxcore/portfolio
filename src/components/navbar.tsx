42313b97d8ddec64442521ceb08b4a62947b56a9
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 17:47:24 -0400

    Initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function navbar(){
+    return(
+        <div>
+            <ul>
+                <li><Link to="./App">Home</Link></li>
+                <li><Link to="/about-me">About me</Link></li>
+                <li><Link to="/portfolio">Portfolio</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default navbar;
