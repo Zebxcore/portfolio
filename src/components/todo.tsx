232d15fdd8aee2998eda1282b9832bac82e6b7cf
Sebastian Cortes <scortesz@udel.edu>
2021-09-23 20:57:53 -0400

    Added wee 3 info + week 4 plan

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -5,20 +5,31 @@ function toDo(){
         <div>
             <h1 className="header"> TODO </h1>
 
+            <h3>Week #4 TODO</h3>
+            <div>
+                <ul>
+                    <p>
+                        For this week I plan to finish all remaining projects/tasks I have yet to start and determine a final 3rd project to add to the portfolio.
+                    </p>
+                </ul>
+            </div>
+
             <h3>Week #3 TODO</h3>
             <div>
                 <ul>
                     <li className="done"> Practice more HTML and read on more advance tags </li>
                     <li className="done"> Start implementing CSS styles to Portfolio website (Want to check <a href="https://www.youtube.com/watch?v=yfoY53QXEnI" target="_blank" rel="noreferrer">this</a> video out later) </li>
                     <li className="done"> Read and watch more tutorials on Javascript/Typescript </li>
-                    <li> Read and watch tutorials for React and hooks </li>
+                    <li className="done"> Read and watch tutorials for React and hooks </li>
                     <li className="done"> Read and watch tutorials for Boostrap (Maybe watch <a href="https://www.youtube.com/watch?v=k32voqQhODc" target="_blank" rel="noreferrer">this</a> video for flexboxing) </li>
                     <li> Start flashcard game website with Typescript/CSS/HTML and start section with links for future projects </li>
-                    <li> Implementation of more advance techniques and functionality like Javascript/Typescript</li>
+                    <li className="done"> Implementation of more advance techniques and functionality like Javascript/Typescript</li>
                     <li> Contemplating the possibility of making a Discord bot on Typescript and create a webpage to add the bot to a private server using APIs</li>
-                    <li> Import this website to a React App</li>
-                    <li> Start exploring the possibility of doing Git Issues and branching</li>
-                    <li> . . .</li>
+                    <li className="done"> Import this website to a React App</li>
+                    <li> Start exploring the possibility of doing Git Issues and branching (In Progress)</li>
+                    <li> <strong>NEW!</strong> Further Investigate hooks, props and how to render props (<a href="https://www.youtube.com/watch?v=Z5iWr6Srsj8" target="_blank" rel="noreferrer">this</a>) videos seems interesting (In Progress)</li>
+                    <li> <strong>NEW!</strong> Watch videos on APIs and how they work so I can include them in my Bot Project</li>
+                    <li> <strong>NEW!</strong> Research local storage and how to handle animations for different types of objects</li>
                 </ul>
             </div>
 
@@ -41,12 +52,12 @@ function toDo(){
             <h3>Week #1 TODO</h3>
             <div>
                 <ul>
-                    <li> Practice more HTML and read on more advance tags </li>
-                    <li> Start implementing CSS styles to Portfolio website (Want to check <a href="https://www.youtube.com/watch?v=yfoY53QXEnI" target="_blank" rel="noreferrer">this</a> video out later) </li>
-                    <li> Read and watch more tutorials on Javascript/Typescript </li>
-                    <li> Read and watch tutorials for React and hooks </li>
-                    <li> Read and watch tutorials for Boostrap (Maybe watch <a href="https://www.youtube.com/watch?v=k32voqQhODc" target="_blank" rel="noreferrer">this</a> video for flexboxing) </li>
-                    <li> Start flashcard game website with Typescript/CSS/HTML and start section with links for future projects </li>
+                    <li className="done"> Practice more HTML and read on more advance tags </li>
+                    <li className="done"> Start implementing CSS styles to Portfolio website (Want to check <a href="https://www.youtube.com/watch?v=yfoY53QXEnI" target="_blank" rel="noreferrer">this</a> video out later) </li>
+                    <li className="done"> Read and watch more tutorials on Javascript/Typescript </li>
+                    <li className="done"> Read and watch tutorials for React and hooks </li>
+                    <li className="done"> Read and watch tutorials for Boostrap (Maybe watch <a href="https://www.youtube.com/watch?v=k32voqQhODc" target="_blank" rel="noreferrer">this</a> video for flexboxing) </li>
+                    <li className="done"> Start flashcard game website with Typescript/CSS/HTML and start section with links for future projects </li>
                     <li> Implementation of more advance techniques and functionality like Javascript/Typescript</li>
                 </ul>
             </div>
