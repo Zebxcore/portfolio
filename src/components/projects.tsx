8e1f324dd90ce3ecc63f159d5afc621ea8dde71d
Sebastian Cortes <scortesz@udel.edu>
2021-10-01 00:16:31 -0400

    fixed links opening on same page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -10,8 +10,8 @@ function projects(){
             </p>
 
             <ul>
-                <li> Flashcard Game that will teach Languages while having fun! Link to the website <a href="https://zebxcore.github.io/Flashcard-game/">here</a> and the repo <a href="https://github.com/Zebxcore/Flashcard-game"> here</a>. (In development)</li>
-                <li> Discord Bot that will reproduce music by using APIs! Link to the website <a href="">here</a> and the repo <a href="https://github.com/Zebxcore/Saory-the-bot"> here</a>. (In development)</li>
+                <li> Flashcard Game that will teach Languages while having fun! Link to the website <a href="https://zebxcore.github.io/Flashcard-game/" target="_blank" rel="noreferrer">here</a> and the repo <a href="https://github.com/Zebxcore/Flashcard-game" target="_blank" rel="noreferrer"> here</a>. (In development)</li>
+                <li> Discord Bot that will reproduce music by using APIs! Link to the website <a href="">here</a> and the repo <a href="https://github.com/Zebxcore/Saory-the-bot" target="_blank" rel="noreferrer"> here</a>. (In development)</li>
             </ul>
         </div>
     )
